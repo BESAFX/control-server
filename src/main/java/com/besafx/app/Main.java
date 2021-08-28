392f7dde7fd5e10104f056ee5e54aed7dade10c9
Bassam Almahdy <bassam.almahdy.1990@gmail.com>
2021-08-28 02:33:02 +0200

    Adding message info to notify user about server state

--- src/main/java/com/besafx/app/Main.java
+++ src/main/java/com/besafx/app/Main.java
@@ -1,13 +1,22 @@
 package com.besafx.app;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import javax.swing.*;
 
 @SpringBootApplication
-public class Main {
+public class Main implements ApplicationRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        final SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
+        builder.headless(false).run(args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) {
+        JOptionPane.showMessageDialog(null, "Control server is up and running now.", "Control Server", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
